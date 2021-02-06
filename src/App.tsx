7afe01578f313141890c6991e97a73c6bdedabce
guivictorr <guilhermevictor20@gmail.com>
2021-02-06 18:02:59 -0300

    chore(config): initial config for style signin page

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { auth } from './firebase';
+// import { useAuthState } from 'react-firebase-hooks/auth';
+import { ThemeProvider } from 'styled-components';
+// import { auth } from './firebase';
+import { theme } from './utils/theme.json';
 
 import GlobalStyles from './styles/global';
 
-import ChatRoom from './pages/ChatRoom';
+// import ChatRoom from './pages/ChatRoom';
 import SignIn from './pages/SignIn';
 
 const App: React.FC = () => {
-  const [user] = useAuthState(auth);
-
+  // const [user] = useAuthState(auth);
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyles />
-      {user ? <ChatRoom /> : <SignIn />}
-    </>
+      <SignIn />
+    </ThemeProvider>
   );
 };
 
