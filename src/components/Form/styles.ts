c0d8b0e01a0ab33ceafac87e100fcdefeece674a
guivictorr <guilhermevictor20@gmail.com>
2021-02-07 02:01:38 -0300

    feat(form): created form component

---
+++ src/components/Form/styles.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const Container = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 340px;
+  margin-top: 70px;
+
+  header {
+    h1 {
+      font-size: 18px;
+      color: ${({ theme }) => theme.colors.secondary};
+    }
+
+    span {
+      font-size: 48px;
+      color: ${({ theme }) => theme.colors.primary};
+    }
+  }
+`;
